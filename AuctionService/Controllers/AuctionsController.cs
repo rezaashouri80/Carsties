984969df20f1cc586dbac37d4bbd741d7a67d3f2
Donyaye Rayaneh <Donyaye Rayaneh@DESKTOP-NT09VC2>
2025-08-21 20:52:17 +0330

    Add project files.

---
+++ AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,101 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions.Include(x => x.Item)
+                .OrderBy(a => a.Item.Make).ToListAsync();
+
+            return _mapper.Map<List<AuctionDto>>(auctions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions.Include(a => a.Item)
+                .FirstOrDefaultAsync(a => a.Id.Equals(id));
+
+            if (auction == null)
+                return NotFound();
+
+            return _mapper.Map<AuctionDto>(auction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+
+            auction.Seller = "test";
+
+            await _context.Auctions.AddAsync(auction);
+
+            var result = _context.SaveChanges() > 0;
+
+            if (!result)
+                return BadRequest("coul not save to db");
+
+            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+        {
+            var auction = await _context.Auctions.Include(b => b.Item)
+                .FirstOrDefaultAsync(a => a.Id.Equals(id));
+
+            if (auction == null) return NotFound();
+
+            auction.Item.Make = updateAuctionDto.Make ?? updateAuctionDto.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? updateAuctionDto.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? updateAuctionDto.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? updateAuctionDto.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? updateAuctionDto.Year;
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Problem Saving Changes");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
+
+            if (auction == null) return NotFound();
+
+            _context.Auctions.Remove(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Problem Saving Changes");
+        }
+    }
+}
---
+++ AuctionService/Program.cs
@@ -0,0 +1,49 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+using AuctionService.RequestHelpers;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
+builder.Services.AddAutoMapper(cfg => {
+    cfg.AddProfile<MappingProfiles>();
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
